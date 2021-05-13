b7e84eb1b7017109bca0dcd180e9b1ad97f2b0a5
giulialiotti <giuliana.aliotti@hotmail.com>
2021-05-13 15:30:36 -0300

    Connection data db

---
+++ src/db/connectionData.js
@@ -0,0 +1,12 @@
+const conf_db_host = 'localhost', // host
+    conf_db_name = 'delilah_resto', // database name
+    conf_user = 'root',           // user name
+    conf_password = '',               // password
+    conf_port = '3306';           // port number
+    
+module.exports = {
+    conf_db_name   : conf_db_name,
+    conf_user     : conf_user,
+    conf_password : conf_password,
+    conf_port     : conf_port
+};
--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,13 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('mysql://root@localhost:3306/delilah_resto');
+const db_data = require('./connectionData');
+const Sequelize   = require('sequelize');
+const sequelize   = new Sequelize( db_data.conf_db_name, db_data.conf_user, db_data.conf_password, { 
+    host: db_data.conf_db_host,
+    dialect: 'mysql',
+    port: db_data.conf_port,
+    dialectOptions: {
+        multipleStatements: true
+    }
+});
 
 sequelize.authenticate().then(() => {
     console.log('Database successfully connected.');
